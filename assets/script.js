7c2fd88da484549fbd66f2df35b2c2775bc009b2
Taani Maama <taani1maama@live.com>
2020-11-06 18:41:04 -0800

    Added my search bar submit event and then added my first function handleSearchRequest.

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// Search Bar Event
+// Calls on function handleSearchRequest when something is submited.
+$('.input-group').on('submit', '.searchBtn', handleSearchRequest)
+
+function handleSearchRequest(e) {
+    e.preventDefault()
+    
+}
