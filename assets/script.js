76b7f1e18518c783e8b2bc0de24a1efaa48644a6
Taani Maama <taani1maama@live.com>
2020-11-07 18:31:51 -0800

    Added Humidity to the display.

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,7 @@ $('.input-group-append').on('click', '#searchBtn', handleSearchRequest)
 let recentSearches = localStorage.getItem('Cities') || [];
 for (let cityIndex = 0; cityIndex < localStorage.length; cityIndex++) {
     let cityList = $('<li>').addClass('list-group-item').text(recentSearches)
+    // cityList.addClass()
     $('#recentSearches').append(cityList)
 }
 
@@ -17,22 +18,17 @@ function handleSearchRequest(e) {
     e.preventDefault()
 
     // LET searchRequest equal the text in the input search bar.
-    let searchRequest = $('#searchInput').val().toUpperCase(0)
-    console.log(searchRequest)
-    // THEN store the value into our localStorage.
-    localStorage.setItem('Cities', searchRequest)
+    let searchRequest = $('#searchInput').val() 
 
     const queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
       "q=" + searchRequest + "&appid=" + APIKey;
 
-    handleForecastInfo(searchRequest, queryURL)
+    handleForecastInfo(queryURL)
     // handle5DayForecast(searchRequest)
 }
 
 // For handling all the forecast info we have this function that will create and append the info in our HTML.
-function handleForecastInfo(searchRequest, queryURL) {
-    console.log(queryURL)
-    let cityCapitalized = searchRequest
+function handleForecastInfo(queryURL) {
 
     // FIRST we need to make ajax call to GET the information we are seeking.
     $.ajax({
@@ -42,13 +38,16 @@ function handleForecastInfo(searchRequest, queryURL) {
 
     // THEN we are going to push our 'response into a function which is going to create and display our information for the requested city.
     .then(function(response) {
+        // We are going to store the city searched for after they have searched for a valid city.
+        localStorage.setItem('Cities', response.name)
         // Display the City Name in the cityId ID.
         $('#cityId').text(response.name + response.weather.icon)
         // Display the response temperature in the temperature ID.
         // First we need to change the temp from K to F
         let tempF = (response.main.temp - 273.15) * 1.80 + 32;
-        $('#temperature').text('Temperature: ' + tempF.toFixed(2))
-
+        $('#temperature').text('Temperature: ' + tempF.toFixed(2) + '°F')
+        // Display the humidity in the humidity ID.
+        $('#humidity').text('Humidity: ' + response.main.humidity + '%')
     })
 
 }
